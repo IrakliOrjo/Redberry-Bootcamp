3209717a8738f5bb60cf78283ac18c8027b4b979
Irakli <irakliorjo92@gmail.com>
2023-02-03 18:03:44 +0400

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Logo from './images/LOGO-023.png'
+import Logo2 from './images/LOGO-401.png'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className='flex flex-col bg-[url("./images/background.png")] min-h-[100vh] 
+      object-cover pt-[1.5625em] justify-center items-center' >
+        <img className='absolute top-[25px] left-[70px] ' src={Logo} alt='logo'/>
+        <div className='absolute top-[89px] w-[91%]  mx-auto  h-[2px] bg-[#1A1A1A]'></div>
+        
+        <div className='absolute w-[299px] h-[299px] bg-[url("./images/LOGO-401.png")] top-[19.5625em] left-[56.25em] mix-blend-multiply z-0'>
+          
+        </div>
+        <img className='absolute hidden top-[19.5625em] left-[57.25em] mix-blend-multiply' src={Logo2} alt='logo2' />
+        <button className='bg-[#1A1A1A] rounded-[8px] 
+        w-[29rem] h-[3.75rem] text-white font-[500] text-[20px] relative '>რეზიუმეს დამატება</button>
+      
+      </div>
+      
     </div>
   );
 }
